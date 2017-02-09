9411c2d1850beb93e0190e2096b21d004ea1b9c2
Dennis Ploeger <dennis.ploeger@kps.com>
2017-02-09 16:50:46 +0100

    v.1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # node-rest-client-promise
 node-rest-client, but with promises
+
+## Introduction
+
+I found this question and answer on StackOverflow about promisifying
+the node-rest-client module:
+
+http://stackoverflow.com/questions/30112657/how-to-use-bluebird-to-promisify-node-rest-client
+
+However, it seems the original author didn't share a node module
+for this, so I did that. Additionally, I had to fix a few small things
+and document it.
+
+This module provides a promisifed version of the
+[node-rest-client](https://www.npmjs.com/package/node-rest-client).
+
+## Usage
+
+This module provides this small function:
+  * client(options): Call `new Client(options)`, promisify and return it
+
+The promisified client methods will have a "Promise" suffix:
+
+```javascript
+var client = require('node-rest-client-promise').Client(options);
+
+client.getPromise(url).catch(...).then(...)
+```
+
+Please note, that the resolved callback of the Promise will be called
+with one argument to comply with Promise style. This argument will
+be an object with the keys 'data' and 'response' reflecting the original
+arguments of e.g. `client.get()`.
+
+For details about node-rest-client, please see the
+[original documentation](https://www.npmjs.com/package/node-rest-client)
---
+++ index.js
@@ -0,0 +1,73 @@
+var Promise = require("bluebird");
+var methodNamesToPromisify = "get post put del patch".split(" ");
+var nodeRestClient = require('node-rest-client');
+
+/**
+ * The Promisifier promisifying the node-rest-client
+ * @param originalMethod Original method to be promisified
+ * @returns {promisified} The promisified version of the method
+ * @constructor
+ */
+function EventEmitterPromisifier(originalMethod) {
+    return function promisified() {
+        var args = [].slice.call(arguments);
+        var self = this;
+        return new Promise(function (resolve, reject) {
+
+            // add the callback to the arguments
+
+            args.push(
+                function (data, response) {
+                    // resolve the Promise providing data and response as
+                    // an object
+                    resolve({
+                        data: data,
+                        response: response
+                    });
+                }
+            );
+
+            // call the method
+
+            var emitter = originalMethod.apply(self,
+                args
+            );
+
+            // listen to specific events leading to rejects
+
+            emitter
+                .on("error", function (err) {
+                    reject(err);
+                })
+                .on("requestTimeout", function () {
+                    reject(new Promise.TimeoutError());
+                })
+                .on("responseTimeout", function () {
+                    reject(new Promise.TimeoutError());
+                });
+        });
+    };
+};
+
+/**
+ * A simple wrapper around `new Client(options)`, returning a promisified
+ * client object.
+ *
+ * @param options Options for `node-rest-client.Client`
+ * @returns {*} the promisified client
+ */
+var client = function (options) {
+
+    var restClient = new nodeRestClient.Client(options);
+
+    var promisifiedClient = Promise.promisifyAll(restClient, {
+        filter: function (name) {
+            return methodNamesToPromisify.indexOf(name) > -1;
+        },
+        promisifier: EventEmitterPromisifier,
+        suffix: 'Promise'
+    });
+    return promisifiedClient;
+}
+
+exports.client = client;
